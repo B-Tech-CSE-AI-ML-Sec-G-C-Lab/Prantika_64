7c0cb300fc0cf5c8a6af0f3b516e1a225620fa8b
Prantika Biswas <priyasankarbiswas@gmail.com>
2026-08-25 22:05:25 +0530

    Add files via upload

---
+++ assignment3(18-08-2026)/return_true.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+
+    if (a == 30 || b == 30 || a + b == 30)
+        cout << "True";
+    else
+        cout << "False";
+
+    return 0;
+}
